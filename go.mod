50b8bdaf048f9fab83b19e9386de27690317057a
Jorge L <jorge.alfmur@gmail.com>
2020-06-03 12:40:30 -0500

    server and client code

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.3
+	github.com/joho/godotenv v1.3.0
 	google.golang.org/grpc v1.29.1
 )
--- server/server.go
+++ server/server.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"os"
 
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/jorgeAM/bloGrpc/blogpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	url := os.Getenv("GRPC_SERVER_HOST")
+	lis, err := net.Listen("tcp", url)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	blogpb.RegisterBlogServiceServer(s, &server{})
+	log.Println("Serving grpc server ...")
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve grpc server: %v", err)
+	}
 }
